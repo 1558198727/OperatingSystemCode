b22b2464f8f8a4e22a15fd1fcb772dc73b988e87
codingyunhao <li_yunhao@outlook.com>
2018-04-26 18:50:11 +0800

    实验四

--- empty1/main.cpp
+++ empty1/main.cpp
@@ -1,31 +1,107 @@
-
 #include <stdio.h>
-#include <iostream>
 #include <stdlib.h>
-#include <sched.h>
-
-#include <sys/time.h>
 #include <sys/types.h>
-#include <sys/unistd.h>
-#include <sys/resource.h>
-using namespace std;
-int main(int argc, char *argv[])  {
-    int i,j,status;
-    int pid[3];
-    struct sched_param p[3];
-    cout<<"pid "<<(pid[0]=fork())<<endl;
-    cout<<"pid "<<(pid[1]=fork())<<endl;
-    cout<<"pid "<<(pid[2]=fork())<<endl;
-        //if((pid[i]=fork()) >0){;
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <sys/msg.h>
 
-        //} else{
+#define BUFSZ   256    //建立或获取 ipc 的一组函数的原型说明
 
-         //       printf("Child PID = %d \n",getpid());
-         //       sleep(1);
-        //
-         //   exit( EXIT_SUCCESS);
-       // }
-cout<<"pid "<<endl;
+int get_ipc_id(char *proc_file,key_t key);
+char *set_shm(key_t shm_key,int shm_num,int shm_flag);
+int set_msq(key_t msq_key,int msq_flag);
+int set_sem(key_t sem_key,int sem_val,int sem_flag); int down(int sem_id);
+int up(int sem_id);
 
+/*信号灯控制用的共同体*/
+typedef union semuns {
+    int val;
+} Sem_uns;
+
+/* 消息结构体*/
+typedef struct msgbuf {
+    long mtype;
+    char mtext[1];
+} Msg_buf;
+
+//生产消费者共享缓冲区即其有关的变量
+key_t buff_key;  int buff_num;  char *buff_ptr;
+
+//生产者放产品位置的共享指针
+key_t pput_key;  int pput_num;
+int *pput_ptr;
+
+//消费者取产品位置的共享指针
+key_t cget_key;  int cget_num;
+int *cget_ptr;
+
+//生产者有关的信号量
+key_t prod_key;
+key_t pmtx_key;
+int prod_sem;
+int pmtx_sem;
+
+//消费者有关的信号量
+key_t cons_key;
+key_t cmtx_key;
+int cons_sem;
+int cmtx_sem;
+int sem_val;
+int sem_flg;
+int shm_flg;
+ #include "ipc.h"
+int get_ipc_id(char *proc_file,key_t key) {
+    FILE *pf;
+    int i,j;
+    char line[BUFSZ],colum[BUFSZ];
+    if((pf = fopen(proc_file,"r")) == NULL){
+        perror("Proc file not open");
+        exit(EXIT_FAILURE);
+    }
+    fgets(line, BUFSZ,pf);
+    while(!feof(pf)){
+        i = j = 0;
+        fgets(line, BUFSZ,pf);
+        while(line[i] == ' ') i++;
+        while(line[i] !=' ')
+            colum[j++] = line[i++];
+            colum[j] = '\0';
+        if(atoi(colum) != key)
+            continue;
+        j=0;
+        while(line[i] == ' ')
+            i++;
+        while(line[i] !=' ')
+            colum[j++] = line[i++];
+        colum[j] = '\0';
+        i = atoi(colum);
+        fclose(pf);
+        return i;
+    }
+    fclose(pf);
+    return -1;
+}
+int down(int sem_id) {
+    struct sembuf buf;
+    buf.sem_op = -1;
+    buf.sem_num = 0;
+    buf.sem_flg = SEM_UNDO;
+    if((semop(sem_id,&buf,1)) <0) {
+        perror("down error ");
+        exit(EXIT_FAILURE);
+    }
+    return EXIT_SUCCESS;
+}
+
+int up(int sem_id) {
+    struct sembuf buf;
+    buf.sem_op = 1;
+    buf.sem_num = 0;
+    buf.sem_flg = SEM_UNDO;
+    if((semop(sem_id,&buf,1)) <0) {
+        perror("up error ");
+        exit(EXIT_FAILURE);
+    }
     return EXIT_SUCCESS;
 }
